8f1762f59264a219e991592d20b96da55b5d0257
shukuchi23 <vaizerd23@gmail.com>
2020-12-18 13:22:43 +0300

    fizzBuzzzz

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,14 +14,11 @@ public class App
         for (int i = 1; i <= 100; i++) {
             if (i % 15 == 0)
                 logger.info("FizzBuzz");
-//                System.out.println();
             else {
                 if (i % 3 == 0)
                     logger.info("Fizz");
-//                    System.out.println("Fizz");
                 if (i % 5 == 0)
                     logger.info("Buzz");
-//                    System.out.println("Buzz");
             }
         }
     }
